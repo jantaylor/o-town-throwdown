55d03acea505fce5808845b830592f3615ca2ab4
Jan Taylor <janttaylor@gmail.com>
2017-03-29 10:39:23 -0600

    New Version and Images
    New version with different framework and updated images and design from
    Tyler.

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/janttaylor/pen/17d3bf6b1ffa5cb8dc11af614113f8e7.
+
+ 
---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('.dropdown-button').dropdown({
+    belowOrigin: true, //If true, the dropdown will show below the activator.
+  });
+  $('.parallax').parallax();
+  $('.scrollspy').scrollSpy();
+});
